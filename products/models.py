494fe91c2db85df4d96c430912fd564be2765dcd
Rafael Mello <rafaelmello0715@outlook.com>
2020-04-20 07:39:58 -0300

    Main product listing page developed

--- products/models.py
+++ products/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=20, blank=True, unique=True)
+    image = models.ImageField(upload_to='products', blank=True, null=True)
+    cost = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'products'
+
+    @property
+    def image_url(self):
+        return settings.MEDIA_URL + str(self.image)
+
+    def __str__(self):
+        return self.name
+
+
+@receiver(pre_save, sender=Product)
+def set_slug_on_pre_save(sender, instance, **kwargs):
+    instance.slug = slugify(instance.name)
